6d0d827bb838ec2fda7bd2d7f420b0e321a85c8f
mathews <chibuluma.mathews@gmail.com>
2024-02-16 19:13:45 +0200

    Removed dummy object

--- eRegister.API/Controllers/AttendanceController.cs
+++ eRegister.API/Controllers/AttendanceController.cs
@@ -14,21 +14,12 @@ public class AttendanceController : ControllerBase {
     [Route("MarkAttendance")]
     public async Task<ActionResult> MarkAttendance([FromBody] Attendance attendance){
 
-     var dummmyAttendance = new Attendance(){
-        SchoolCode= "SICT",
-        DepartmentCode = "CS",
-        ProgrammeCode = "42",
-        CourseCode =  "CS350",
-        Sin= 1234,
-        DateAttended = "2024",
-        IsPresent = 0
-     };
-        // if (attendance == null){
-        //     Console.WriteLine("You are reaching here but it looks like the object null");
-        //     return NotFound("You are reaching here but it looks like the object null"); // to be revised later
-        // }
+        if (attendance == null){
+            Console.WriteLine("You are reaching here but it looks like the object null");
+            return NotFound("You are reaching here but it looks like the object null"); // to be revised later
+        }
 
-        context.Attendances.Add(dummmyAttendance);
+        context.Attendances.Add(attendance);
         await context.SaveChangesAsync(); // saves to db
 
         return CreatedAtAction("MarkAttendance", null);
