6a85945d9fc305fb5a4555f27cc5bd23537d3df0
mathews <chibuluma.mathews@gmail.com>
2024-02-16 17:59:32 +0200

    Scaffolded dbcontext from sqlite db

---
+++ eRegister.DAL/Models/Attendance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eRegister.DAL.Models;
+
+public partial class Attendance
+{
+    public int Id { get; set; }
+
+    public string SchoolCode { get; set; } = null!;
+
+    public string DepartmentCode { get; set; } = null!;
+
+    public string ProgrammeCode { get; set; } = null!;
+
+    public string CourseCode { get; set; } = null!;
+
+    public int Sin { get; set; }
+
+    public string DateAttended { get; set; } = null!;
+
+    public int? IsPresent { get; set; }
+}
---
+++ eRegister.DAL/Models/ERegisterDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace eRegister.DAL.Models;
+
+public partial class ERegisterDbContext : DbContext
+{
+    public ERegisterDbContext()
+    {
+    }
+
+    public ERegisterDbContext(DbContextOptions<ERegisterDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Attendance> Attendances { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=../eRegister_DB.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Attendance>(entity =>
+        {
+            entity.ToTable("Attendance");
+
+            entity.Property(e => e.Sin).HasColumnName("SIN");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
